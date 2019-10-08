140e799af640ab0cfdde6ba043f5eda36e90cf97
ConnorSheremeta <sheremet@ualberta.ca>
2019-10-08 13:23:33 -0600

    Create a Custom PMH Routing Constraint [ch46]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+class PMHConstraint
+  PMHVERBS = %w(Identify ListSets ListMetadataFormats ListRecords GetRecord ListIdentifiers).freeze
+
+  def matches?(request)
+    request.query_parameters.key?(:verb) && PMHVERBS.include?(request.query_parameters[:verb])
+  end
+end
+
 Oaisys::Engine.routes.draw do
-  match '/', to: 'pmh#endpoint', via: [:get, :post]
+  match '/', to: 'pmh#endpoint', via: [:get, :post], constraints: PMHConstraint.new
 end
