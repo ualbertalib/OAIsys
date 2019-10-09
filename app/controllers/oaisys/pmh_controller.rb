ddb5b6116760ac54be72cd5895f10ec7fe0ee65d
ConnorSheremeta <sheremet@ualberta.ca>
2019-10-09 11:28:04 -0600

    Modify Constraints to Handle Verb Dispatch [ch28]

--- app/controllers/oaisys/pmh_controller.rb
+++ app/controllers/oaisys/pmh_controller.rb
@@ -2,5 +2,7 @@ require_dependency "oaisys/application_controller"
 
 module Oaisys
   class PMHController < ApplicationController
+    def list_identifiers
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,42 @@ class PMHConstraint
   end
 end
 
+class IdentifyConstraint < PMHConstraint
+  def matches?(request)
+    super && request.query_parameters[:verb] == 'Identify'
+  end
+end
+
+class ListSetsConstraint < PMHConstraint
+  def matches?(request)
+    super && request.query_parameters[:verb] == 'ListSets'
+  end
+end
+
+class ListMetadataFormatsConstraint < PMHConstraint
+  def matches?(request)
+    super && request.query_parameters[:verb] == 'ListMetadataFormats'
+  end
+end
+
+class ListRecordsConstraint < PMHConstraint
+  def matches?(request)
+    super && request.query_parameters[:verb] == 'ListRecords'
+  end
+end
+
+class GetRecordConstraint < PMHConstraint
+  def matches?(request)
+    super && request.query_parameters[:verb] == 'GetRecord'
+  end
+end
+
+class ListIdentifiersConstraint < PMHConstraint
+  def matches?(request)
+    super && request.query_parameters[:verb] == 'ListIdentifiers'
+  end
+end
+
 Oaisys::Engine.routes.draw do
-  match '/', to: 'pmh#endpoint', via: [:get, :post], constraints: PMHConstraint.new
+  match '/', to: 'pmh#list_identifiers', via: [:get, :post], constraints: ListIdentifiersConstraint.new
 end
