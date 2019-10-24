260977b156946e0bb918697fab5e870310d6721a
ConnorSheremeta <sheremet@ualberta.ca>
2019-10-24 11:41:22 -0600

    fixed /lib require

--- app/controllers/oaisys/application_controller.rb
+++ app/controllers/oaisys/application_controller.rb
@@ -2,7 +2,7 @@ module Oaisys
   # TODO: Extending off base rather than api because render wouldn't find views, look into another way to resolve this.
   class ApplicationController < ActionController::Base
     # protect_from_forgery with: :exception
-    require '../../lib/exceptions'
+    require 'oaisys/exceptions'
 
     rescue_from 'Oaisys::PMHError' do |exception|
       respond_to do |format|
