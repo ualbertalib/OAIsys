dd91c3de3ca4881ed527cdb508a0ba1c29a2813d
ConnorSheremeta <sheremet@ualberta.ca>
2019-10-31 13:48:50 -0600

    Add tests and fix rubocop violations

---
+++ test/integration/expect_args_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class NavigationTest < ActionDispatch::IntegrationTest
+
+  include Oaisys::Engine.routes.url_helpers
+
+  setup do
+    @routes = Oaisys::Engine.routes
+  end
+
+  def test_unexpected_arg_xml
+    get oaisys_path + '?verb=ListMetadataFormats&nastyParam=nasty', headers: { 'Accept' => 'application/xml' }
+
+    schema = Nokogiri::XML::Schema(File.open(file_fixture('OAI-PMH.xsd')))
+    document = Nokogiri::XML(@response.body)
+    assert_empty schema.validate(document)
+
+    assert_select 'OAI-PMH' do
+      assert_select 'responseDate'
+      assert_select 'request'
+      assert_select 'error', I18n.t('error_messages.illegal_or_missing_arguments')
+    end
+  end
+
+  def test_missing_required_arg_xml
+    # Missing required metadataPrefix param
+    get oaisys_path + '?verb=ListRecords', headers: { 'Accept' => 'application/xml' }
+
+    schema = Nokogiri::XML::Schema(File.open(file_fixture('OAI-PMH.xsd')))
+    document = Nokogiri::XML(@response.body)
+    assert_empty schema.validate(document)
+
+    assert_select 'OAI-PMH' do
+      assert_select 'responseDate'
+      assert_select 'request'
+      assert_select 'error', I18n.t('error_messages.illegal_or_missing_arguments')
+    end
+  end
+
+end
