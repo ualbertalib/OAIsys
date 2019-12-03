54d3b9e7446dff181ed7406d2881b7634928e984
ConnorSheremeta <sheremet@ualberta.ca>
2019-12-02 17:01:03 -0700

    put in proper file

--- lib/oaisys.rb
+++ lib/oaisys.rb
@@ -4,7 +4,4 @@ module Oaisys
   require 'oaisys/bad_argument_error'
   require 'oaisys/cannot_disseminate_format_error'
   require 'oaisys/no_records_match_error'
-
-  # Set an action on unpermitted parameters to raise an exception, used to validate parameters.
-  ActionController::Parameters.action_on_unpermitted_parameters = :raise
 end
