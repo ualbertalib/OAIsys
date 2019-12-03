98c43def64fa77a9167eda46201664ba9b2c98d8
ConnorSheremeta <sheremet@ualberta.ca>
2019-12-03 12:06:26 -0700

    add to test setup for action on unpermitted parameters config

--- test/integration/identify_test.rb
+++ test/integration/identify_test.rb
@@ -6,6 +6,8 @@ class IdentifyTest < ActionDispatch::IntegrationTest
 
   setup do
     @routes = Oaisys::Engine.routes
+    # Set an action on unpermitted parameters to raise an exception, used to validate parameters.
+    ActionController::Parameters.action_on_unpermitted_parameters = :raise
   end
 
   def test_identify_xml
