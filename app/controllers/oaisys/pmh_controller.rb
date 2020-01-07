c1e1315fc0e6c276346544fac5fd687ac8456eae
Connor Sheremeta <sheremet@ualberta.ca>
2020-01-06 17:18:33 -0700

    Verb ListIdentifiers [ch101] (#17)

--- app/controllers/oaisys/pmh_controller.rb
+++ app/controllers/oaisys/pmh_controller.rb
@@ -17,59 +17,75 @@ class Oaisys::PMHController < Oaisys::ApplicationController
   def bad_verb; end
 
   def identify
-    expect_no_args for_verb: :Identify
-
     respond_to do |format|
       format.xml { render :identify }
     end
   end
 
   def list_sets
-    expect_args for_verb: :ListSets, exclusive: [:resumptionToken]
+    expect_args exclusive: [:resumptionToken]
   end
 
   # TODO: Handle the identifier argument.
   def list_metadata_formats
-    expect_args for_verb: :ListMetadataFormats, optional: [:identifier]
+    parameters = expect_args optional: [:identifier]
 
     respond_to do |format|
       format.xml do
-        render :list_metadata_formats, locals: { supported_formats: SUPPORTED_FORMATS }
+        render :list_metadata_formats, locals: { supported_formats: SUPPORTED_FORMATS, parameters: parameters }
       end
     end
   end
 
   def list_records
-    expect_args for_verb: :ListRecords, required: [:metadataPrefix], optional: [:from, :until, :set],
-                exclusive: [:resumptionToken]
+    expect_args required: [:metadataPrefix], optional: [:from, :until, :set], exclusive: [:resumptionToken]
   end
 
   # get_record is referring to the verb, not a getter.
   # rubocop:disable Naming/AccessorMethodName
   def get_record
-    expect_args for_verb: :GetRecord, required: [:identifier, :metadataPrefix]
+    expect_args required: [:identifier, :metadataPrefix]
   end
   # rubocop:enable Naming/AccessorMethodName
 
+  # TODO: Handle from, until, and resumptionToken arguments.
   def list_identifiers
-    expect_args for_verb: :ListIdentifiers, required: [:metadataPrefix], optional: [:from, :until, :set],
-                exclusive: [:resumptionToken]
+    parameters = expect_args required: [:metadataPrefix], optional: [:from, :until, :set], exclusive: [:resumptionToken]
+    identifiers = if params[:set].present?
+                    public_items_for_metadata_format(verb: parameters[:verb], format: params[:metadataPrefix],
+                                                     restricted_to_set: params[:set])
+                      .pluck(:id, :record_created_at, :member_of_paths)
+                  else
+                    public_items_for_metadata_format(verb: parameters[:verb], format: params[:metadataPrefix])
+                      .pluck(:id, :record_created_at, :member_of_paths)
+                  end
+    raise Oaisys::NoRecordsMatchError.new(parameters: parameters.slice(:verb, :metadataPrefix)) if identifiers.empty?
+
+    respond_to do |format|
+      format.xml do
+        render :list_identifiers, locals: { identifiers: identifiers, parameters: parameters }
+      end
+    end
   end
 
   private
 
-  def expect_args(for_verb:, required: [], optional: [], exclusive: [])
-    arguments = params.except('verb', 'controller', 'action').keys.map(&:to_sym)
-    expected_verb_arguments = required + optional + exclusive
-    unexpected_arguments = (arguments - expected_verb_arguments).present?
-    missing_required_arguments = (required - arguments).present?
-
-    raise Oaisys::BadArgumentError.new(for_verb: for_verb) if unexpected_arguments || missing_required_arguments
+  def expect_args(required: [], optional: [], exclusive: [])
+    params.require([:verb] + required)
+    params.permit([:verb] + required + optional + exclusive).to_h
   end
 
-  # Conventional way of calling expect_args for a verb with no arguments.
-  def expect_no_args(for_verb:)
-    expect_args(for_verb: for_verb)
+  def public_items_for_metadata_format(verb:, format:, restricted_to_set: nil)
+    model = case format
+            when 'oai_dc'
+              Oaisys::Engine.config.oai_dc_model
+            when 'oai_etdms'
+              Oaisys::Engine.config.oai_etdms_model
+            else
+              raise Oaisys::CannotDisseminateFormatError.new(parameters: { verb: verb, metadataPrefix: format })
+            end
+
+    !restricted_to_set.nil? ? model.public_items.belongs_to_path(restricted_to_set.tr(':', '/')) : model.public_items
   end
 
 end
--- lib/oaisys.rb
+++ lib/oaisys.rb
@@ -1,6 +1,7 @@
-require 'oaisys/engine'
-
 module Oaisys
+  require 'oaisys/engine'
   require 'oaisys/pmh_error'
   require 'oaisys/bad_argument_error'
+  require 'oaisys/cannot_disseminate_format_error'
+  require 'oaisys/no_records_match_error'
 end
