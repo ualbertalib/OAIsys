887bbf916d4d3e3898651f3fa01db1dc67c902ea
Connor Sheremeta <sheremet@ualberta.ca>
2020-08-18 10:47:55 -0600

    Removed oaisys xml header requirement (#42)

--- app/controllers/oaisys/application_controller.rb
+++ app/controllers/oaisys/application_controller.rb
@@ -2,26 +2,18 @@
 class Oaisys::ApplicationController < ActionController::Base
 
   rescue_from 'ActionController::ParameterMissing' do
-    respond_to do |format|
-      format.xml do
-        render :error, locals: {
-          # Being unsafe by using to_unsafe_h here because calling .permit will throw this error again.
-          parameters: params.to_unsafe_h.slice(:verb), error_code: :badArgument,
-          error_message: I18n.t('error_messages.illegal_or_missing_arguments')
-        }
-      end
-    end
+    render :error, formats: :xml, locals: {
+      # Being unsafe by using to_unsafe_h here because calling .permit will throw this error again.
+      parameters: params.to_unsafe_h.slice(:verb), error_code: :badArgument,
+      error_message: I18n.t('error_messages.illegal_or_missing_arguments')
+    }
   end
 
   # protect_from_forgery with: :exception
   rescue_from 'Oaisys::PMHError' do |exception|
-    respond_to do |format|
-      format.xml do
-        render :error, locals: {
-          parameters: exception.parameters, error_code: exception.error_code, error_message: exception.error_message
-        }
-      end
-    end
+    render :error, formats: :xml, locals: {
+      parameters: exception.parameters, error_code: exception.error_code, error_message: exception.error_message
+    }
   end
 
 end
