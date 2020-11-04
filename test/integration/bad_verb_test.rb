ef861e3e0d00330acc48903e830d638bb8f77b57
ConnorSheremeta <sheremet@ualberta.ca>
2020-11-04 12:59:30 -0700

    fix tests

--- test/integration/bad_verb_test.rb
+++ test/integration/bad_verb_test.rb
@@ -11,28 +11,42 @@ class BadVerbTest < ActionDispatch::IntegrationTest
   def test_bad_verb_xml
     get oaisys_path(verb: 'nastyVerb'), headers: { 'Accept' => 'application/xml' }
 
-    assert_nasty_verb_response
+    assert_unknown_verb_response
   end
 
   def test_bad_verb_xml_post
-    post oaisys_path(verb: 'nastyVerb'), headers: { 'Content-Type' => 'application/x-www-form-urlencoded',
-                                                    'Content-Length' => 82 }
-    assert_nasty_verb_response
+    post oaisys_path(verb: 'nastyVerb'), headers: { 'Content-Type' => 'application/x-www-form-urlencoded', 'Content-Length' => 82 }
+
+    assert_unknown_verb_response
   end
 
   def test_no_verb_xml
     get oaisys_path, headers: { 'Accept' => 'application/xml' }
 
-    assert_nasty_verb_response
+    assert_no_verb_response
   end
 
   def test_no_verb_xml_post
     post oaisys_path, headers: { 'Content-Type' => 'application/x-www-form-urlencoded', 'Content-Length' => 82 }
 
-    assert_nasty_verb_response
+    assert_no_verb_response
   end
 
-  def assert_nasty_verb_response
+  def assert_unknown_verb_response
+    assert_response :success
+    schema = Nokogiri::XML::Schema(File.open(file_fixture('OAI-PMH.xsd')))
+    document = Nokogiri::XML(@response.body)
+    assert_empty schema.validate(document)
+
+    assert_select 'OAI-PMH' do
+      assert_select 'responseDate'
+      assert_select 'request'
+      assert_select 'error', I18n.t('error_messages.unknown_verb')
+    end
+  end
+
+  def assert_no_verb_response
+    assert_response :success
     schema = Nokogiri::XML::Schema(File.open(file_fixture('OAI-PMH.xsd')))
     document = Nokogiri::XML(@response.body)
     assert_empty schema.validate(document)
